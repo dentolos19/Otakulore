9217455719fc82f1095bb65ee654fa9287b31849
Dennise Catolos <dentolos19@gmail.com>
2022-12-09 17:45:58 +0800

    Updated user interface styles

--- Otakulore/Models/Pages/MainPageModel.cs
+++ Otakulore/Models/Pages/MainPageModel.cs
@@ -8,11 +8,26 @@ namespace Otakulore.Models;
 public partial class MainPageModel : BasePageModel
 {
 
+    [ObservableProperty] private string _version;
     [ObservableProperty] private int _rateRemaining;
 
     public MainPageModel()
     {
         DataService.Instance.Client.RateChanged += (_, args) => RateRemaining = args.RateRemaining;
+        #if ANDROID
+        var buildVersion = VersionTracking.CurrentBuild;
+        #if DEBUG
+        buildVersion = "Debug";
+        #endif
+        Version = $"{VersionTracking.CurrentVersion} ({buildVersion})";
+        #else
+        var version = VersionTracking.CurrentVersion;
+        var buildVersion = version.Split('.')[3];
+        #if DEBUG
+        buildVersion = "Debug";
+        #endif
+        Version =  $"{version.Remove(version.LastIndexOf("."))} ({buildVersion})";
+        #endif
     }
 
 }
