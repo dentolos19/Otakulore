398703079b8aa08a34bd3169ab7694478d34a8ee
Dennise Catolos <dentolos19@gmail.com>
2022-12-15 19:04:52 +0800

    Fixed build error

--- Otakulore/Models/Pages/SettingsPageModel.cs
+++ Otakulore/Models/Pages/SettingsPageModel.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using Otakulore.Helpers;
 using Otakulore.Pages;
 using Otakulore.Services;
 using Otakulore.Utilities.Attributes;
